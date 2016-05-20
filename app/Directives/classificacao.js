2bd9b86daa25fdfc95e7bec4174a3ac8b8d074f2
matheusEdiley <mediley38@gmail.com>
2016-05-19 21:54:35 -0300

    Diretivas

--- app/Directives/classificacao.js
+++ app/Directives/classificacao.js
@@ -2,13 +2,7 @@ var mainApp = angular.module("MainApp");
 
 mainApp.directive('classificacao', function() {
 
-	return {
-		restrict: 'A',
-		link: function(scope, element, attr) {
-			scope.dismiss = function() {
-				element.modal('hide');
-			};
-		}
-	}
+	
+	
 
 });
